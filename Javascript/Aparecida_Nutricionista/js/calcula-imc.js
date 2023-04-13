b7b2338310edf3401e0870c6dd4fa287387e4435
rach-noronha <rach.noronha@gmail.com>
2023-04-13 00:28:44 -0300

    Adição de novas funções da página Aparecida_Nutricionista

--- Javascript/Aparecida_Nutricionista/js/calcula-imc.js
+++ Javascript/Aparecida_Nutricionista/js/calcula-imc.js
@@ -13,17 +13,17 @@ for (var i = 0; i < pacientes.length; i++) {
     var peso = tdPeso.textContent;
     var altura = tdAltura.textContent;
 
-    var alturaEhValida = true;
-    var pesoEhValido = true;
+    var alturaEhValida = validaAltura(altura);
+    var pesoEhValido = validaPeso(peso);
 
-    if (peso <= 0 || peso >= 1000) {
+    if (!pesoEhValido) {
         console.log("Peso inválido!");
         tdPeso.textContent = "Peso inválido!";
         pesoEhValido = false;
         paciente.classList.add("paciente-invalido");
     }
 
-    if (altura <= 0 || altura >= 3) {
+    if (!alturaEhValida) {
         console.log("Altura inválida!");
         tdAltura.textContent = "Altura inválida!";
         alturaEhValida = false;
@@ -31,10 +31,31 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 
     if (alturaEhValida && pesoEhValido) {
-
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
-    } else {
-        tdImc.textContent = "Altura e/ou peso inválidos!"
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
     }
+}
+
+function validaPeso(peso) {
+    if (peso > 0 && peso < 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+    if (altura > 0 && altura <= 3.00) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function calculaImc(peso, altura) {
+    var imc = 0;
+
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
 }
